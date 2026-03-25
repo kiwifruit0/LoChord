c71f96a4a77e10d367e4fda1c4b8d7e2e9cc1413
kiwi <79637319+kiwifruit0@users.noreply.github.com>
2026-03-25 00:13:36 +0000

    can send midi notes

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,18 @@
 #include "USB.h"
-#include "USBCDC.h"
+#include "USBMIDI.h"
 #include <Arduino.h>
-#include <ButtonCtrl.h>
-#include <Control_Surface.h>
 
-USBCDC USBSerial;
-USBDebugMIDI_Interface midi{USBSerial};
+USBMIDI MIDIDevice;
 
 void setup() {
+  MIDIDevice.begin();
   USB.begin();
-  USBSerial.begin(115200);
-
   delay(2000);
-
-  while (!USBSerial) {
-    delay(10);
-  }
-
-  USBSerial.println("USB CDC DIRECT");
 }
 
 void loop() {
-  USBSerial.println("HELLO");
-  delay(1000);
+  MIDIDevice.noteOn(60, 64, 0);
+  delay(500);
+  MIDIDevice.noteOff(60, 0, 0);
+  delay(500);
 }
