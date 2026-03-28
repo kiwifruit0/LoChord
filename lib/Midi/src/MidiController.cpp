15d9e75681897d8876619beb290026e9ae095b62
kiwi <79637319+kiwifruit0@users.noreply.github.com>
2026-03-28 23:10:11 +0000

    structure of midicontroller

--- lib/Midi/src/MidiController.cpp
+++ lib/Midi/src/MidiController.cpp
@@ -1,3 +1,10 @@
 #include "MidiController.h"
 
-USBMIDI MidiController::getMidiDevice() { return _midi; }
+MidiController::MidiController(bool chordMode, float defaultVelocity,
+                               float strumAmt, float randVelocityAmt)
+    : chordMode(chordMode),
+      defaultVelocity(defaultVelocity),
+      strumAmt(strumAmt),
+      randVelocityAmt(randVelocityAmt) {};
+
+USBMIDI MidiController::getMidiDevice() { return this->midi; }
--- lib/Midi/src/MidiController.h
+++ lib/Midi/src/MidiController.h
@@ -3,8 +3,15 @@
 
 class MidiController {
 public:
+  MidiController(bool chordMode = true, float defaultVelocity = 100,
+                 float strumAmt = 0, float randVelocityAmt = 0);
+
   USBMIDI getMidiDevice();
 
 private:
-  USBMIDI _midi;
+  USBMIDI midi;
+  bool chordMode;
+  float strumAmt;
+  float randVelocityAmt;
+  float defaultVelocity;
 };
